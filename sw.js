6bfa1a981c6b922e3eeb1466e6a67aca48a8403e
TrY1222 <masterhannes132@gmail.com>
2026-07-17 11:04:37 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service Worker für "Mein Tag"
 // Strategie: Netzwerk zuerst (damit Updates sofort ankommen),
 // bei Offline-Betrieb Rückgriff auf den Cache.
-const CACHE = "mein-tag-v18";
+const CACHE = "mein-tag-v20";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 
 self.addEventListener("install", e => {
